ec75f1601758f7725f4c07252f21b436cb40a1a1
Sean Callan <seancallan@gmail.com>
2016-12-26 20:56:21 -0700

    Release 0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# v 0.5.0
+
+* Add support for new params: access_type, approval_prompt, state.
+* Fix Elixir warnings
+
 # v 0.4.0
 
 * Target Elixir 1.3 and greater
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule UeberauthGoogle.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @url "https://github.com/ueberauth/ueberauth_google"
 
   def project do
