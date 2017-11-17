3a7949fdc5a007c2a124aa0b6a5bd808688d9a68
Dave Lucia <davelucianyc@gmail.com>
2017-11-17 14:07:01 -0500

    Fix CaseClauseError in fetch_user for {:error, Response} tuple (#43)

--- lib/ueberauth/strategy/google.ex
+++ lib/ueberauth/strategy/google.ex
@@ -128,6 +128,8 @@ defmodule Ueberauth.Strategy.Google do
         set_errors!(conn, [error("token", "unauthorized")])
       {:ok, %OAuth2.Response{status_code: status_code, body: user}} when status_code in 200..399 ->
         put_private(conn, :google_user, user)
+      {:error, %OAuth2.Response{status_code: status_code}} ->
+        set_errors!(conn, [error("OAuth2", status_code)])
       {:error, %OAuth2.Error{reason: reason}} ->
         set_errors!(conn, [error("OAuth2", reason)])
     end
