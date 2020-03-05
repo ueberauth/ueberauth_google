174cba04ca61bb257fddd0ac5a3255ef73150504
Matt Pinkston <matt@tablecheck.com>
2020-03-05 18:44:14 +0900

    added tests

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,11 @@
 use Mix.Config
+
+config :ueberauth, Ueberauth,
+  providers: [
+    google: {Ueberauth.Strategy.Google, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: "client_id",
+  client_secret: "client_secret",
+  token_url: "token_url"
