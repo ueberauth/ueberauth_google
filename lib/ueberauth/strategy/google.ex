b0d0a6c5e4f8aac328df63e99f0c4fe8631c688a
Aleksandr Basov <bainst4@gmail.com>
2020-05-06 23:14:02 +0500

    Use dynamic env and remove unused deps

--- lib/ueberauth/strategy/google.ex
+++ lib/ueberauth/strategy/google.ex
@@ -13,8 +13,6 @@ defmodule Ueberauth.Strategy.Google do
   alias Ueberauth.Auth.Credentials
   alias Ueberauth.Auth.Extra
 
-  @allowed_client_ids Application.get_env(:ueberauth, Ueberauth.Strategy.Google.OAuth)[:allowed_client_ids]
-
   @doc """
   Handles initial request for Google authentication.
   """
@@ -206,7 +204,7 @@ defmodule Ueberauth.Strategy.Google do
     Keyword.get(options(conn), key, Keyword.get(default_options(), key))
   end
 
-  def verify_token(conn, client, id_token) do
+  def verify_token(_conn, client, id_token) do
     url = "https://www.googleapis.com/oauth2/v3/tokeninfo"
     params = %{"id_token" => id_token}
     resp = OAuth2.Client.get(client, url, [], params: params)
@@ -215,7 +213,7 @@ defmodule Ueberauth.Strategy.Google do
       {:ok, %OAuth2.Response{status_code: 200,
         body: %{"aud" => aud} = body
       }} ->
-        if Enum.member?(@allowed_client_ids, aud) do
+        if Enum.member?(allowed_client_ids(), aud) do
           {:ok, body}
         else
           {:error, "Unknown client id #{aud}"}
@@ -223,5 +221,9 @@ defmodule Ueberauth.Strategy.Google do
       _ ->
         {:error, "Token verification failed"}
     end
+  end
+
+  defp allowed_client_ids() do
+    Application.get_env(:ueberauth, Ueberauth.Strategy.Google.OAuth)[:allowed_client_ids]
   end
 end
