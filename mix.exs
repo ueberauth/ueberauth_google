5454e68c8ef2161453b30a2eecfd6bbbb0dd625a
Aaron Tinio <aptinio@gmail.com>
2023-11-14 09:18:42 +0800

    chore: add changelog

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule UeberauthGoogle.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/ueberauth/ueberauth_google"
-  @version "0.12.0"
+  @version "0.12.1"
 
   def project do
     [
